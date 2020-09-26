17f6d34b4fa39ba9885825d93844bdb72d12da74
chainvisions <chainvisions@outlook.com>
2020-09-26 13:24:12 -0600

    Fix compiler errors in ZingToken

--- contracts/ZingToken.sol
+++ contracts/ZingToken.sol
@@ -3,8 +3,20 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract ZingToken is ERC20("ZingToken", "ZING"), Ownable {
+contract ZingToken is ERC20, Ownable {
+
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) public {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+    }
+
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
     }
+
 }
